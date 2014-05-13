810fc67e720603a642e6701d6c565de6edfe9220
Christopher Kintner <kintner@gmail.com>
2014-05-13 18:53:40 -0500

    added callback to set hash for Hit

--- app/models/hit.rb
+++ app/models/hit.rb
@@ -1,5 +1,16 @@
 class Hit < Sequel::Model
 
+  def before_create
+    set_hash
+  end
 
+  def calculate_hash
+    data = values.slice(:id, :url, :referrer, :created_at).delete_if { |k,v| v.nil?}
+    Digest::MD5.hexdigest(data.to_s)
+  end
+
+  def set_hash
+    self.hash = calculate_hash
+  end
 
 end
