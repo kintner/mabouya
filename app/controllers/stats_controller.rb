bd8a7984b0ff83c9249bc15bf070aa546f0a6d72
Christopher Kintner <kintner@gmail.com>
2014-05-14 23:06:42 -0500

    added api methods

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,2 +1,12 @@
 class StatsController < ApplicationController
+
+  def top_urls
+    render json: Hit.top_urls
+  end
+
+  def top_referrers
+    render json: Hit.top_referrers
+  end
+
+
 end
--- app/models/hit.rb
+++ app/models/hit.rb
@@ -27,4 +27,32 @@ class Hit < Sequel::Model
     self.hash = calculate_hash
   end
 
+
+
+  def self.top_urls(start_date = Date.new(2014,01,01), end_date = Date.new(2014,1,5))
+    Hit.db["SELECT url, count(url) as visits, date FROM hits
+            WHERE date BETWEEN ? and ?
+            GROUP BY url, date order by date, visits", start_date, end_date].to_a
+
+
+  end
+
+  def self.top_referrers(start_date = Date.new(2014,01,01), end_date = Date.new(2014,1,5))
+    top_urls = top_urls(start_date, end_date)
+    result = {}
+    top_urls.group_by {|x| x[:date]}.each do |date, urls|
+      result[date] ||= []
+      urls.sort {|x,y| x[:visits] <=> y[:visits]}[0..9].each do |url|
+        referrers = Hit.db["SELECT referrer, count(referrer) as visits from hits where date = ? and url = ? group by referrer order by count(referrer) desc limit 5", date, url[:url]].to_a.map {|row| {url: row[:referrer], visits: row[:visits]}}
+        record = {url:url[:url], visits: url[:visits], referrers: referrers }
+        result[date] << record
+      end
+    end
+
+    result
+
+
+  end
+
+
 end
