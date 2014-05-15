7ed03a03226bd9a8a213928f611d6ca19847c75a
Christopher Kintner <kintner@gmail.com>
2014-05-14 19:22:52 -0500

    added stats controller

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  get 'top_urls' => 'stats#top_urls'
+  get 'top_referrers' => 'stats#top_referrers'
+
 end
